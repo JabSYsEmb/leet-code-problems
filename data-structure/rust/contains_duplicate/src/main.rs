37bcb588d7047b040cc572d03aa7e80c1360c4a2
JabSysEmb <abduljabbar.sha97@gmail.com>
2022-08-04 16:15:11 +0300

    BinaryTree implement and rustfmt config toml added

--- data-structure/rust/contains_duplicate/src/main.rs
+++ data-structure/rust/contains_duplicate/src/main.rs
@@ -1,105 +1,149 @@
-/*
-    Given an integer array nums, return true if any value appears at least 
-    twice in the array, and return false if every element is distinct.
+#![allow(dead_code)]
 
-    Example 1:
-
-    Input: nums = [1,2,3,1]
-    Output: true
-    Example 2:
-
-    Input: nums = [1,2,3,4]
-    Output: false
-    Example 3:
-
-    Input: nums = [1,1,1,3,3,4,3,2,4,2]
-    Output: true
-     
-
-    Constraints:
-
-    1 <= nums.length <= 105
-    -109 <= nums[i] <= 109
-*/
-
-struct Node<'a>{
-    data: &'a i32,
-    next: Option<Box<Node<'a>>>,
+enum TraversalOrder {
+    InOrder,
+    PreOrder,
+    PostOrder,
 }
 
-impl<'a> Node<'a>{
-    fn new(val: &'a i32) -> Self
-    {
-        return Node{
+struct Node<'a> {
+    data: &'a i32,
+    lnode: Option<Box<Node<'a>>>,
+    rnode: Option<Box<Node<'a>>>,
+}
+
+struct BinaryTree<'a> {
+    root_node: Option<Node<'a>>,
+}
+
+impl<'a> Node<'a> {
+    fn new(val: &'a i32) -> Self {
+        Node {
             data: val,
-            next: None,
+            lnode: None,
+            rnode: None,
         }
     }
-
-    fn insert(&mut self, val: &'a i32) 
-    {
-        let target_node = &mut self.next;
+    fn insert(
+        &mut self,
+        val: &'a i32,
+    ) {
+        let target_node = if val > self.data {
+            &mut self.rnode
+        } else {
+            &mut self.lnode
+        };
         match target_node {
             &mut Some(ref mut next_node) => next_node.insert(val),
             &mut None => *target_node = Some(Box::new(Node::new(val))),
         }
     }
 
-    fn sort(&mut self) 
-    {
-        let temp = self.data;
-        let mut next_node = &mut self.next;
-    }
-
-    fn insert_at_pos(&self, pos: &mut i32) 
-    {
-        let mut temp = &self.next;
-        let mut current_node: &Option<Box<Node>>; 
-        while *pos != 0{
-            match temp.as_ref(){
-                Some(node) => current_node = &node.next,
-                _ => {},
-            }
-            println!("{pos}");
-            *pos -= 1;
-        }
-    }
-
-    fn to_print(&self) 
-    {
+    fn to_print(&self) {
         print!("{} ", self.data);
-        match &self.next{
-            Some(ref next_node) => {
-                print!("-> ");
-                next_node.to_print();
+    }
+
+    fn in_order(&self) {
+        match &self.lnode {
+            Some(ref next_lnode) => {
+                next_lnode.in_order();
             },
-            _ => println!(""),
+            _ => {},
+        }
+        self.to_print();
+        match &self.rnode {
+            Some(ref next_rnode) => {
+                next_rnode.in_order();
+            },
+            _ => {},
         }
     }
 
+    fn pre_order(&self) {
+        self.to_print();
+        match &self.lnode {
+            Some(ref next_lnode) => {
+                next_lnode.pre_order();
+            },
+            _ => {},
+        }
+        match &self.rnode {
+            Some(ref next_rnode) => {
+                next_rnode.pre_order();
+            },
+            _ => {},
+        }
+    }
+
+    fn post_order(&self) {
+        match &self.lnode {
+            Some(ref next_lnode) => {
+                next_lnode.post_order();
+            },
+            _ => {},
+        }
+        match &self.rnode {
+            Some(ref next_rnode) => {
+                next_rnode.post_order();
+            },
+            _ => {},
+        }
+        self.to_print();
+    }
 }
 
-struct Solution;
-impl Solution{
-    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-        return false;
+impl<'a> BinaryTree<'a> {
+    pub fn new(val: &'a i32) -> Self {
+        let root = Some(Node::new(val));
+        return {
+            BinaryTree {
+                root_node: root,
+            }
+        };
+    }
+
+    fn insert(
+        &mut self,
+        val: &'a i32,
+    ) {
+        match &mut self.root_node {
+            Some(ref mut node) => node.insert(val),
+            _ => {},
+        }
+    }
+
+    fn to_print(
+        self,
+        order: TraversalOrder,
+    ) {
+        match self.root_node {
+            Some(ref node) => match order {
+                TraversalOrder::InOrder => {
+                    print!("in-order   : ");
+                    node.in_order();
+                    println!();
+                },
+                TraversalOrder::PreOrder => {
+                    print!("pre-order  : ");
+                    node.pre_order();
+                    println!();
+                },
+                TraversalOrder::PostOrder => {
+                    print!("post-order : ");
+                    node.post_order();
+                    println!();
+                },
+            },
+            _ => println!("something went error"),
+        }
     }
 }
 
 fn main() {
-    let vec_arr: Vec<i32> = vec![1,234,34,2,45,1,2,3,23,23,1,2,3,5,2,3,1];
-    println!("{:?}", vec_arr);
-    let mut linked_list = Node::new(&vec_arr[0]); 
-    for index in 1..vec_arr.len(){
-        linked_list.insert(&vec_arr[index]);
+    let vec_arr = vec![15, 25, 30, 16, 10, 8, 12, 8, 20, 16, 25];
+    let mut binary_tree = BinaryTree::new(&vec_arr[0]);
+    for index in 1..vec_arr.len() {
+        binary_tree.insert(&vec_arr[index]);
     }
-
-    linked_list.to_print();
-
-    linked_list.insert_at_pos(&mut 7);
-
-    if !Solution::contains_duplicate(vec_arr)
-    {
-        println!("testing");
-    }
+    binary_tree.to_print(TraversalOrder::PostOrder);
 }
