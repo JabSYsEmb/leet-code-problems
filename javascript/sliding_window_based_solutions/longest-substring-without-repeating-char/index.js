b7fdc26b099a23b09081b4410df28c2415be2cec
JabSysEmb <abduljabbar.sha97@gmail.com>
2022-11-01 02:03:42 +0300

    rearrange directories

---
+++ javascript/sliding_window_based_solutions/longest-substring-without-repeating-char/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const length_of_longest_substring = (str) => {
+	if (str.length < 2) return str.length;
+
+	let start = 0;
+	let		end = 1;
+	let   win = (end - start);
+
+	while( end <= str.length & win !== str.length){
+		if(win < (end - start)) win = end - start;
+		if( !str.slice(start, end).includes(str[end]) ) {
+			end += 1;
+			continue;
+		}
+		start += 1;
+	}
+
+	return win;
+}
+
+module.exports = {
+	length_of_longest_substring,
+}
